809aea1aaa58a8057ae677a67f925a4cfa00b081
Diogo Mazurek Pavan <115800955+diogoMPavan@users.noreply.github.com>
2023-03-13 16:47:13 -0300

    Update Even_Fibonacci.java

--- Even_Fibonacci.java
+++ Even_Fibonacci.java
@@ -4,10 +4,20 @@ public class Even_Fibonacci {
 
 	public static void main(String[] args) {
 
-		int a;
+		int sum=0;
 		
-		int[] fib = new int[50];
+		int[] fib = new int[34];
 		
+		fib[0] = 0;
+		fib[1] = 1;
+		
+		for (int i = 2; i < 34; i++) {
+			fib[i] = fib[i-2] + fib[i-1];
+			if (fib[i] % 2 == 0) {
+				sum+= fib[i];
+			}
+		}
+		System.out.println(sum);
 		
 	}
 
